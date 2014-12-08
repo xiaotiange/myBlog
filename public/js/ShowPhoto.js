6d0d03730f5e7f4bbfccafe6b1ad96ade409f9d3
chx <18768113677@163.com>
2014-12-08 20:15:32 +0800

    more

--- public/js/ShowPhoto.js
+++ public/js/ShowPhoto.js
@@ -38,7 +38,7 @@ var yabe = yabe || {};
             }
 
             // check for file size
-            if (oFile.size > 250 * 1024) {
+            if (oFile.size > 750 * 750) {
                 $('.error').html('文件太大！').show();
                 return;
             }
